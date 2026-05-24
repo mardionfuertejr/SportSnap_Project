ae77363ae3f2264afdc9365c243671b4c788dbc1
mardionfuerte <mardionjrcordetafuerte@gmail.com>
2026-05-24 15:55:59 +0800

    Updated some files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import SignUpView
 from .forms import CustomAuthenticationForm
 
 urlpatterns = [
+    path('admin/', admin.site.urls, name='admin'),
     path('register/', SignUpView.as_view(), name='register'),
     path('login/', LoginView.as_view(authentication_form=CustomAuthenticationForm), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', include('gallery.urls')),
 ]
